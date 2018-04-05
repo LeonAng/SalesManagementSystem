f58cd6cdbde941a0b30cebff37a45e9d58747152
Leon <lvang_leon@foxmail.com>
2018-04-05 20:32:40 +0800

    修改readme.md

---
+++ README.md
@@ -0,0 +1,4 @@
+# SalesManagementSystem
+Spring+SpringMVC+MyBatis+Mysql
+<br/>
+销售管理系统
--- src/leon/sms/service/impl/UserServiceImpl.java
+++ src/leon/sms/service/impl/UserServiceImpl.java
@@ -26,5 +26,4 @@ public class UserServiceImpl implements UserService
 	{
 		return userMapper.list();
 	}
-	
 }
